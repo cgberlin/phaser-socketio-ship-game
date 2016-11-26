fc93cca8efb1a61dba861f0b981015a5b82bfcf8
cody <codyberlin@gmail.com>
2016-11-25 16:31:43 -0800

    removed jquery for now

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var http = require('http');
 var express = require('express');
 var socket_io = require('socket.io');
-var $ = require('jquery');
 
 var app = express();
 app.use(express.static('public'));
